188baca1c960d34424c836073ff9b96c32e37173
Al <lin.alice72@gmail.com>
2018-05-04 10:40:38 -0400

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,3 +1,9 @@
 class Comment {
-  
+  constructor(data){
+    this.content = data.content
+    this.id = data.id
+    this.image_id = data.image_id
+    Comment.all.push(this)
+  }
 }
+Comment.all = []
--- src/Image.js
+++ src/Image.js
@@ -1,4 +1,11 @@
 class Image {
-  
+  constructor(data){
+    this.id = data.id
+    this.url = data.url
+    this.like_count = data.like_count
+    this.name = data.name
+    Image.all.push(this)
+  }
 
 }
+Image.all = []
