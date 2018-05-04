7ec85d0c5a6f9da95a9d29be9626563f057d69d0
github username <github email address>
2018-05-04 11:05:09 -0400

    finished

--- src/Image.js
+++ src/Image.js
@@ -1,4 +1,52 @@
 class Image {
-  
 
+  constructor(data){
+    this.id = data.idea
+    this.url = data.url
+    this.name = data.name
+    this.like_count = data.like_count
+    this.comments = data.comments
+    Image.all.push(this)
+  }
+
+
+  static showImage(json){
+    json.forEach(function(element){
+      let name = document.getElementById("name")
+        name.innerText = element.name
+
+       let imagePlace = document.getElementById("image")
+       imagePlace.src = element.url
+      imagePlace.innerHTML += '<img src="'+element.url+'" ></img>';
+
+
+
+      let likeButton = document.getElementById("like_button")
+
+        likeButton.addEventListener("click", function(e){
+
+              let likesCounter = document.getElementById("likes")
+              likesCounter.innerText =+1
+              Adapter.increaseLikes(element.id)
+        })
+
+          let button = document.getElementById("button")
+          let commentForm = document.getElementById("comment_form")
+          button.addEventListener("click", function(e){
+
+                let comments = document.getElementById("comments")
+                let comment = document.createElement("li")
+                comment.setAttribute("id", "comment")
+                let commentInput = document.getElementById("comment_input").value
+
+                    comments.append("commentInput")
+
+                    Adapter.saveComments(element.id, element.comment)
+
+          })
+
+    })
+  }
 }
+
+Image.all = []
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const imageId = 1 //Enter your assigned imageId here
+  const imageId = 41 //Enter your assigned imageId here
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
   const likeURL = `https://randopic.herokuapp.com/likes/`
   const commentsURL = `https://randopic.herokuapp.com/comments/`
+
+
+
+  let imageCard = document.getElementById("image_card")
+
+  Adapter.getImages().then(function(json){
+      Image.showImage([json])
+  })
 
 })
